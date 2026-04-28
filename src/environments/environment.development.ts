44a702655036958b1c83d8b6dac5b094580b28fc
Addicted2u <dayro.rios.p@gmail.com>
2026-04-27 20:50:09 -0500

    Initial commit

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: false,
+  platformProviderApiBaseUrl: 'http://localhost:8080/api/v1',
+  platformProviderProvidersEndpointPath: '/providers',
+  platformProviderProductsEndpointPath: '/products',
+  platformProviderKitsEndpointPath: '/kits',
+  platformProviderBatchesEndpointPath: '/batches',
+  platformProviderSalesEndpointPath: '/sales',
+  platformProviderCategoriesEndpointPath: '/categories',
+  platformProviderDashboardEndpointPath: '/dashboard',
+  platformProviderUsersEndpointPath: '/users',
+  platformProviderAuthEndpointPath: '/authentication',
+  platformProviderAuthSignInEndpointPath: '/authentication/sign-in',
+  platformProviderAuthSignUpEndpointPath: '/authentication/sign-up'
+}
