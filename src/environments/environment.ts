82c569fc8b245fe9cade3edb084993156edfa218
Addicted2u <dayro.rios.p@gmail.com>
2026-05-01 06:12:11 -0500

    fix(environment): toggle API base URL and production flag for development and production environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  production: true,
-  platformProviderApiBaseUrl: 'https://backend-stocktrack-production.up.railway.app/api/v1',
+  production: false,
+  platformProviderApiBaseUrl: 'http://localhost:8080/api/v1',
   platformProviderProductsEndpointPath: '/products',
   platformProviderProvidersEndpointPath: '/providers',
   platformProviderBatchesEndpointPath: '/batches',
